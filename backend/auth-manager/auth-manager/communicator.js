21b0c7781e7ad9b589033f29efd1ec30b8a096c0
AudrenGuillaume <audren.guilllaume@gmail.com>
2018-12-29 23:07:34 +0100

    WIP communicator, pm2

--- backend/auth-manager/auth-manager/communicator.js
+++ backend/auth-manager/auth-manager/communicator.js
@@ -2,10 +2,14 @@ module.exports = function communication(hemera) {
 
     //internal 
     const getTokenMessage = { topic: 'auth-manager', cmd: 'getToken' };
-    const createMessage = { topic: 'auth-manager', cmd: 'getToken' };
-    const createMessage = { topic: 'auth-manager', cmd: 'getToken' };
+    const getLoginMessage = { topic: 'auth-manager', cmd: 'getLogin' };
+    const getRoleMessage = { topic: 'auth-manager', cmd: 'getRole' };
+    const dispatchLoginMessage = { topic: 'auth-manager', cmd: 'dispatchLogin' };
+    const dispatchLogoutMessage = { topic: 'auth-manager', cmd: 'dispatchLogout' };
 
     return {
+
+        getToken: async (query) => await hemera.act({getTokenMessage, query}),
 
     }
 }
