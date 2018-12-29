256b52881ae1479d6ec64bcf2cc3c3c182170b72
Alecsandre-Antoine <diabeul@live.fr>
2018-12-29 22:48:45 +0100

    blabla

--- backend/auth-manager/auth-manager/data-access.js
+++ backend/auth-manager/auth-manager/data-access.js
@@ -1,13 +1,22 @@
 module.exports = (hemera, cfg) => {
 
+    const { collection } = cfg;
 
     return {
         findByLogin: async (token, query) => {
             return await hemera.act({
                 topic: 'sql-store',
                 cmd: 'find',
-                collection: 'user',
+                collection: collection,
                 query: { Login: query.Login }
+            })
+        },
+        findByToken: async (token, query) => {
+            return await hemera.act({
+                topic: 'sql-store',
+                cmd: 'find',
+                collections: collection,
+                query: {} //CACHE MANAGER }
             })
         }
     }
