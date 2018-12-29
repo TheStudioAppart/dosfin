e5acc3d7236c19d8dea45e583dfe2c51fb4c7feb
Alec Stophe <alecstophe@outlook.com>
2018-12-29 21:16:14 +0100

    sql?

--- CreateDatabaseAndTable.sql
+++ CreateDatabaseAndTable.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`TS_GROUP` (
   `Code` VARCHAR(45) NOT NULL,
   `Label` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `CODE_UNIQUE` (`Code` ASC) VISIBLE)
+  UNIQUE INDEX `CODE_UNIQUE` (`Code` ASC))
 ENGINE = InnoDB;
 
 
@@ -61,9 +61,9 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`TC_USER` (
   `Nom` VARCHAR(100) NULL,
   `Prenom` VARCHAR(100) NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Login_UNIQUE` (`Login` ASC) VISIBLE,
-  INDEX `groupUser_idx` (`GroupId` ASC) VISIBLE,
-  INDEX `AddressUser_idx` (`AddressId` ASC) VISIBLE,
+  UNIQUE INDEX `Login_UNIQUE` (`Login` ASC),
+  INDEX `groupUser_idx` (`GroupId` ASC),
+  INDEX `AddressUser_idx` (`AddressId` ASC),
   CONSTRAINT `groupUser`
     FOREIGN KEY (`GroupId`)
     REFERENCES `TheStudioAppart`.`TS_GROUP` (`Id`)
@@ -87,7 +87,7 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`TS_TypeEvent` (
   `Code` VARCHAR(45) NOT NULL,
   `Label` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Code_UNIQUE` (`Code` ASC) VISIBLE)
+  UNIQUE INDEX `Code_UNIQUE` (`Code` ASC))
 ENGINE = InnoDB;
 
 
@@ -101,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`TS_EventStatus` (
   `Code` VARCHAR(45) NULL,
   `Label` VARCHAR(45) NULL,
   PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Code_UNIQUE` (`Code` ASC) VISIBLE)
+  UNIQUE INDEX `Code_UNIQUE` (`Code` ASC))
 ENGINE = InnoDB;
 
 
@@ -135,10 +135,10 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_Event` (
   `StatusId` INT NOT NULL,
   `CTTId` INT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `typeEvent_idx` (`TypeEventId` ASC) VISIBLE,
-  INDEX `statusEvent_idx` (`StatusId` ASC) VISIBLE,
-  INDEX `addressEvent_idx` (`AddressId` ASC) VISIBLE,
-  INDEX `CttEvent_idx` (`CTTId` ASC) VISIBLE,
+  INDEX `typeEvent_idx` (`TypeEventId` ASC),
+  INDEX `statusEvent_idx` (`StatusId` ASC),
+  INDEX `addressEvent_idx` (`AddressId` ASC),
+  INDEX `CttEvent_idx` (`CTTId` ASC),
   CONSTRAINT `typeEvent`
     FOREIGN KEY (`TypeEventId`)
     REFERENCES `TheStudioAppart`.`TS_TypeEvent` (`Id`)
@@ -172,8 +172,8 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_UserInvited` (
   `UserId` INT NULL,
   `EventId` INT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `eventInvited_idx` (`EventId` ASC) VISIBLE,
-  INDEX `userInvited_idx` (`UserId` ASC) VISIBLE,
+  INDEX `eventInvited_idx` (`EventId` ASC),
+  INDEX `userInvited_idx` (`UserId` ASC),
   CONSTRAINT `eventInvited`
     FOREIGN KEY (`EventId`)
     REFERENCES `TheStudioAppart`.`T_Event` (`Id`)
@@ -197,8 +197,8 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_EventParticipant` (
   `UserId` INT NULL,
   `EventId` INT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `eventParticipantevent_idx` (`EventId` ASC) VISIBLE,
-  INDEX `eventParticipantUser_idx` (`UserId` ASC) VISIBLE,
+  INDEX `eventParticipantevent_idx` (`EventId` ASC),
+  INDEX `eventParticipantUser_idx` (`UserId` ASC),
   CONSTRAINT `eventParticipantevent`
     FOREIGN KEY (`EventId`)
     REFERENCES `TheStudioAppart`.`T_Event` (`Id`)
@@ -223,8 +223,8 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_CTTParticipant` (
   `UserId` INT NULL,
   `CTTId` INT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `eventParticipantUser_idx` (`UserId` ASC) VISIBLE,
-  INDEX `cttParticipantctt_idx` (`CTTId` ASC) VISIBLE,
+  INDEX `eventParticipantUser_idx` (`UserId` ASC),
+  INDEX `cttParticipantctt_idx` (`CTTId` ASC),
   CONSTRAINT `cttParticipantctt`
     FOREIGN KEY (`CTTId`)
     REFERENCES `TheStudioAppart`.`T_CTT` (`Id`)
@@ -248,7 +248,7 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_ListAchat` (
   `Id` INT NOT NULL AUTO_INCREMENT,
   `CttId` INT NOT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `cttList_idx` (`CttId` ASC) VISIBLE,
+  INDEX `cttList_idx` (`CttId` ASC),
   CONSTRAINT `cttList`
     FOREIGN KEY (`CttId`)
     REFERENCES `TheStudioAppart`.`T_CTT` (`Id`)
@@ -281,8 +281,8 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_ListAchatItem` (
   `ProduitId` INT NULL,
   `Quantite` INT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `ListAchat_idx` (`ListAchatId` ASC) VISIBLE,
-  INDEX `produit_idx` (`ProduitId` ASC) VISIBLE,
+  INDEX `ListAchat_idx` (`ListAchatId` ASC),
+  INDEX `produit_idx` (`ProduitId` ASC),
   CONSTRAINT `ListAchat`
     FOREIGN KEY (`ListAchatId`)
     REFERENCES `TheStudioAppart`.`T_ListAchat` (`Id`)
@@ -310,10 +310,10 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_Depense` (
   `AcheteurId` INT NULL,
   `T_Depensecol` VARCHAR(45) NULL,
   PRIMARY KEY (`Id`),
-  INDEX `cttdepense_idx` (`CttId` ASC) VISIBLE,
-  INDEX `listachatitem_idx` (`AchaItemId` ASC) VISIBLE,
-  INDEX `listachat_idx` (`ListAchatId` ASC) VISIBLE,
-  INDEX `acheteur_idx` (`AcheteurId` ASC) VISIBLE,
+  INDEX `cttdepense_idx` (`CttId` ASC),
+  INDEX `listachatitem_idx` (`AchaItemId` ASC),
+  INDEX `listachat_idx` (`ListAchatId` ASC),
+  INDEX `acheteur_idx` (`AcheteurId` ASC),
   CONSTRAINT `cttdepense`
     FOREIGN KEY (`CttId`)
     REFERENCES `TheStudioAppart`.`T_CTT` (`Id`)
@@ -347,8 +347,8 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_DepenseParticipant` (
   `DepenseId` INT NOT NULL,
   `ParticipantId` INT NOT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `depenseparticipant_idx` (`ParticipantId` ASC) VISIBLE,
-  INDEX `depensedepense_idx` (`DepenseId` ASC) VISIBLE,
+  INDEX `depenseparticipant_idx` (`ParticipantId` ASC),
+  INDEX `depensedepense_idx` (`DepenseId` ASC),
   CONSTRAINT `depenseparticipant`
     FOREIGN KEY (`ParticipantId`)
     REFERENCES `TheStudioAppart`.`T_CTTParticipant` (`Id`)
@@ -372,7 +372,7 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`T_StockItem` (
   `Quantite` INT NOT NULL,
   `ProduitId` INT NOT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `stockitemproduit_idx` (`ProduitId` ASC) VISIBLE,
+  INDEX `stockitemproduit_idx` (`ProduitId` ASC),
   CONSTRAINT `stockitemproduit`
     FOREIGN KEY (`ProduitId`)
     REFERENCES `TheStudioAppart`.`T_Produit` (`Id`)
@@ -391,7 +391,7 @@ CREATE TABLE IF NOT EXISTS `TheStudioAppart`.`TS_Stock` (
   `Nom` VARCHAR(45) NULL,
   `StockId` INT NOT NULL,
   PRIMARY KEY (`Id`),
-  INDEX `Stock_idx` (`StockId` ASC) VISIBLE,
+  INDEX `Stock_idx` (`StockId` ASC),
   CONSTRAINT `Stock`
     FOREIGN KEY (`StockId`)
     REFERENCES `TheStudioAppart`.`TS_Stock` (`Id`)
